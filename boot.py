09fb8f6f561164d2db42ae7c27cfce92288b2b7c
Michael Mogenson <michael.mogenson@gmail.com>
2019-07-02 15:16:17 -0400

    odroid go display

--- boot.py
+++ boot.py
@@ -1,6 +1,7 @@
 import network
 import machine
 import sys
+import display
 
 sys.path[1] = '/flash/lib'
 
@@ -10,6 +11,12 @@ if not wlan.isconnected():
     wlan.connect('ssid', 'password')
     while not wlan.isconnected():
         pass
+
+print('IP addr:', wlan.ifconfig()[0])
+tft = display.TFT()
+tft.init(tft.ILI9341, width=240, height=320, miso=19, clk=18, mosi=23,
+         dc=21, cs=5, backl_pin=14, backl_on=1, rot=tft.LANDSCAPE_FLIP)
+tft.text(0,0, wlan.ifconfig()[0])
 
 rtc = machine.RTC()
 if not rtc.synced():
